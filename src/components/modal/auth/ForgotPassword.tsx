55a23784de350d01e2083cef622c2ef48ca05f6a
muhammad kashif zia <kashif@idenbrid.com>
2025-03-18 15:43:35 +0500

    code push

--- src/components/modal/auth/ForgotPassword.tsx
+++ src/components/modal/auth/ForgotPassword.tsx
@@ -38,7 +38,7 @@ const ForgotPasswordModal = ({ isOpen, onClose }: ForgotPasswordModalProps) => {
   };
 
   return (
-    <div className="fixed inset-0 right-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+    <div className="fixed inset-0 right-0 bg-[#10182885] flex justify-center items-center z-50">
       <div className="bg-white p-6 rounded-lg w-full max-w-[546px] shadow-lg relative">
         {!isOtpSent ? (
           <>
